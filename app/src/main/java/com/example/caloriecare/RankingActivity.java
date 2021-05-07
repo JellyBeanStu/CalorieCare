c543af9a38b65a5ddddb913c325780593ece4b0a
WonHyuk22 <chanceday12@naver.com>
2021-05-07 09:57:43 +0900

    Create Navi bar

--- app/src/main/java/com/example/caloriecare/RankingActivity.java
+++ app/src/main/java/com/example/caloriecare/RankingActivity.java
@@ -2,7 +2,9 @@ package com.example.caloriecare;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
 
 public class RankingActivity extends AppCompatActivity {
 
@@ -11,5 +13,29 @@ public class RankingActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_ranking);
         setTitle("rank");
+    }
+    public void Click1(View v){
+        Intent intent = new Intent(this,MainActivity.class);
+        startActivity(intent);
+        overridePendingTransition(0,0);
+        finish();
+    }
+    public void Click2(View v){
+        Intent intent = new Intent(this,RankingActivity.class);
+        startActivity(intent);
+        overridePendingTransition(0,0);
+        finish();
+    }
+    public void Click3(View v){
+        Intent intent = new Intent(this,StatisticActivity.class);
+        startActivity(intent);
+        overridePendingTransition(0,0);
+        finish();
+    }
+    public void Click4(View v){
+        Intent intent = new Intent(this,ProfileActivity.class);
+        startActivity(intent);
+        overridePendingTransition(0,0);
+        finish();
     }
 }

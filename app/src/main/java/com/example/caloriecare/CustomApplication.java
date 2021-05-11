eaa45c5f02232bb90a527ad068e97c42b0be383c
wnsdud4761 <62161544+wnsdud4761@users.noreply.github.com>
2021-05-11 18:19:10 +0900

    Update CustomApplication.java

--- app/src/main/java/com/example/caloriecare/CustomApplication.java
+++ app/src/main/java/com/example/caloriecare/CustomApplication.java
@@ -1,90 +1,56 @@
 package com.example.caloriecare;
 
-import android.app.Application;
-import android.content.Context;
+import androidx.appcompat.app.AppCompatActivity;
 
-import com.kakao.auth.ApprovalType;
-import com.kakao.auth.AuthType;
-import com.kakao.auth.IApplicationConfig;
-import com.kakao.auth.ISessionConfig;
-import com.kakao.auth.KakaoAdapter;
-import com.kakao.auth.KakaoSDK;
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.ImageView;
+import android.widget.TextView;
 
-public class CustomApplication extends Application
-{
-    private static volatile CustomApplication instance = null;
+import com.bumptech.glide.Glide;
+import com.kakao.usermgmt.UserManagement;
+import com.kakao.usermgmt.callback.LogoutResponseCallback;
 
-    private static class KakaoSDKAdapter extends KakaoAdapter
-    {
-        // 카카오 로그인 세션을 불러올 때의 설정값을 설정하는 부분.
-        public ISessionConfig getSessionConfig() {
+public class SubActivity extends AppCompatActivity {
 
-            return new ISessionConfig() {
-                @Override
-                public AuthType[] getAuthTypes() {
-                    return new AuthType[] {AuthType.KAKAO_LOGIN_ALL};
-                    //로그인을 어떤 방식으로 할지 지정
-                    //KAKAO_LOGIN_ALL: 모든 로그인방식을 사용하고 싶을때 지정.
-                }
-
-                @Override
-                public boolean isUsingWebviewTimer() {
-                    return false;
-                    // SDK 로그인시 사용되는 WebView에서 pause와 resume시에 Timer를 설정하여 CPU소모를 절약한다.
-                    // true 를 리턴할경우 webview로그인을 사용하는 화면서 모든 webview에 onPause와 onResume 시에 Timer를 설정해 주어야 한다.
-                    // 지정하지 않을 시 false로 설정된다.
-                }
-
-                @Override
-                public boolean isSecureMode() {
-                    return false;
-                    // 로그인시 access token과 refresh token을 저장할 때의 암호화 여부를 결정한다.
-                }
-
-                @Override
-                public ApprovalType getApprovalType() {
-                    return ApprovalType.INDIVIDUAL;
-                    // 일반 사용자가 아닌 Kakao와 제휴된 앱에서만 사용되는 값으로, 값을 채워주지 않을경우 ApprovalType.INDIVIDUAL 값을 사용하게 된다.
-                }
-
-                @Override
-                public boolean isSaveFormData() {
-                    return true;
-                    // Kakao SDK 에서 사용되는 WebView에서 email 입력폼의 데이터를 저장할지 여부를 결정한다.
-                    // true일 경우, 다음번에 다시 로그인 시 email 폼을 누르면 이전에 입력했던 이메일이 나타난다.
-                }
-            };
-        }
-
-        @Override
-        public IApplicationConfig getApplicationConfig() {
-            return new IApplicationConfig() {
-                @Override
-                public Context getApplicationContext() {
-                    return CustomApplication.getGlobalApplicationContext();
-                }
-            };
-        }
-    }
-
-    public static CustomApplication getGlobalApplicationContext() {
-        if(instance == null) {
-            throw new IllegalStateException("this application does not inherit com.kakao.GlobalApplication");
-        }
-        return instance;
-    }
+    private String strNick, strProfileImg, strEmail;
 
     @Override
-    public void onCreate() {
-        super.onCreate();
-        instance = this;
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_sub);
 
-        KakaoSDK.init(new KakaoSDKAdapter());
-    }
+        Intent intent = getIntent();
+        strNick = intent.getStringExtra("name");
+        strEmail = intent.getStringExtra("email");
+        strProfileImg = intent.getStringExtra("profileImg");
 
-    @Override
-    public void onTerminate() {
-        super.onTerminate();
-        instance = null;
+        TextView tv_nick = findViewById(R.id.tv_nickName);
+        TextView tv_email = findViewById(R.id.tv_email);
+
+        ImageView tv_profile = findViewById(R.id.tv_profile);
+
+        tv_nick.setText(strNick); //닉네임
+        tv_email.setText(strEmail); // 이메일
+
+        Glide.with(this).load(strProfileImg).into(tv_profile);//이미지
+
+        findViewById(R.id.btn_logout).setOnClickListener(new View.OnClickListener()
+        {
+            @Override
+            public void onClick(View view)
+            {
+                UserManagement.getInstance().requestLogout(new LogoutResponseCallback()
+                {
+                    @Override
+                    public void onCompleteLogout()
+                    {
+                        // 로그아웃 성공시 수행하는 지점
+                        finish(); // 현재 액티비티 종료
+                    }
+                });
+            }
+        });
     }
 }
